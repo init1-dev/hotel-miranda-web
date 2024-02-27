5f8f74b6fe4caffebf555541b154f81f827a7e74
init1-dev <init1.dev@gmail.com>
2024-02-27 20:40:21 +0100

    ADD: swiper food sliders basic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+
 document.addEventListener("DOMContentLoaded", () => {
 
     // HAMBURGUER

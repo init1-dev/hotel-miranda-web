c19909f13f4061f2b2b48e77b6483ff97c6ecc29
init1-dev <init1.dev@gmail.com>
2024-03-02 04:54:48 +0100

    turned back from spa to normal website

--- js/main.js
+++ js/main.js
@@ -22,26 +22,6 @@ const playVideo = () => {
     }
 }
 
-const loadPage = (url) => {
-    const uniqueTimeParam = new Date().getTime();
-    const newUrl = url + '?time=' + uniqueTimeParam;
-    const pageContent = document.getElementById('page-content');
-
-    pageContent.classList.add('loading');
-    
-    fetch(newUrl)
-        .then(response => response.text())
-        .then(html => {
-            pageContent.innerHTML = html;
-
-            document.getElementById('page-content').innerHTML = html;
-            video = document.querySelector('.luxuryPlace__video') || '';
-            playVideo();
-            initializeSwiper();
-            scrollToTop();
-        });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.getElementById("header__menu-toggle");
     const menu = document.querySelector(".header__nav--list");
@@ -58,15 +38,6 @@ document.addEventListener("DOMContentLoaded", () => {
             menuToggle.checked = false;
         }
     });
-
-    setTimeout(() => {
-        const form = document.getElementById('check-availability-form');
-    
-        form.addEventListener('submit', (e) => {
-            e.preventDefault();
-            loadPage('room-list.html');
-        })
-    }, 200);
 
     $(document).ready(function() {
         let lastScrollTop = 0;
@@ -99,106 +70,94 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-
-    loadPage('home.html')
-    
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            loadPage(this.href);
-        });
-    });
-
 });
 
-const initializeSwiper = () => {
-    const roomSwiper = new Swiper("#rooms-swiper", {
-        grabCursor: true,
-        spaceBetween: 40,
-        slidesPerView: 1,
-        initialSlide: 1,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            1000: {
-                spaceBetween: 30,
-                addSlidesBefore: 1,
-                slidesPerView: 3,
-                centeredSlides: true,
-            }
+const roomSwiper = new Swiper("#rooms-swiper", {
+    grabCursor: true,
+    spaceBetween: 40,
+    slidesPerView: 1,
+    initialSlide: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1000: {
+            spaceBetween: 30,
+            addSlidesBefore: 1,
+            slidesPerView: 3,
+            centeredSlides: true,
         }
-    });
+    }
+});
 
-    const roomSwiperNormal = new Swiper("#rooms-swiper-normal", {
-        grabCursor: true,
-        spaceBetween: 40,
-        slidesPerView: 1,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            1000: {
-                spaceBetween: 30,
-                initialSlide: 1,
-                slidesPerView: 3,
-                centeredSlides: true,
-            }
+const roomSwiperNormal = new Swiper("#rooms-swiper-normal", {
+    grabCursor: true,
+    spaceBetween: 40,
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1000: {
+            spaceBetween: 30,
+            initialSlide: 1,
+            slidesPerView: 3,
+            centeredSlides: true,
         }
-    });
-    
-    const coreFeaturesSwiper = new Swiper('#coreFeatures-swipper', {
-        slidesPerView: 1,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            1000: {
-                slidesPerView: 3,
-                grid: {
-                    rows: 2,
-                    fill: 'row',
-                },
-                spaceBetween: 100,
-            }
+    }
+});
+
+const coreFeaturesSwiper = new Swiper('#coreFeatures-swipper', {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+            grid: {
+                rows: 2,
+                fill: 'row',
+            },
+            spaceBetween: 100,
         }
-    });
-    
-    const foodMenuSwipper = new Swiper('#menu-swipper', {
-        slidesPerView: 1,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        spaceBetween: 40,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            1000: {
-                slidesPerView: 2,
-                spaceBetween: 100,
-                autoHeight: true,
-            }
+    }
+});
+
+const foodMenuSwipper = new Swiper('#menu-swipper', {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    spaceBetween: 40,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1000: {
+            slidesPerView: 2,
+            spaceBetween: 100,
+            autoHeight: true,
         }
-    });
-    
-    const foodImageSwipper = new Swiper('#images-swipper', {
-        slidesPerView: 1,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            1000: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-                autoHeight: true,
-            }
+    }
+});
+
+const foodImageSwipper = new Swiper('#images-swipper', {
+    slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        1000: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            autoHeight: true,
         }
-    });
-}
+    }
+});
